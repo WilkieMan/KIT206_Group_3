6935f73fd78c111e854909cda3375ce95c67260e
WilkieMan <jake.w.wilkinson@gmail.com>
2023-12-06 15:07:58 +1100

    Added fake data and title enumerations

--- KIT206_RAP/KIT206_RAP/FakeReasearcherData.cs
+++ KIT206_RAP/KIT206_RAP/FakeReasearcherData.cs
@@ -8,5 +8,19 @@ namespace KIT206_RAP
 {
     internal class FakeReasearcherData
     {
+        public List<Researcher> Generate()
+        {
+            return new List<Researcher>()
+            {
+                new Researcher(Researcher.Title.Dr, "Albert", "Einstein"),
+                new Researcher(Researcher.Title.Dr, "Marie", "Curie"),
+                new Researcher(Researcher.Title.Mrs, "Jane", "Doe"),
+                new Researcher(Researcher.Title.Mr, "John", "Doe"),
+                new Researcher(Researcher.Title.Prof, "Stephen", "Hawkings"),
+                new Researcher(Researcher.Title.Ms, "Amelia", "Reagan"),
+                new Researcher(Researcher.Title.Mrs, "Roberta", "Dawson"),
+                new Researcher(Researcher.Title.Rev, "Mark", "Holland")
+            };
+        }
     }
 }
--- KIT206_RAP/KIT206_RAP/Researcher.cs
+++ KIT206_RAP/KIT206_RAP/Researcher.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,12 +9,27 @@ namespace KIT206_RAP
 {
     internal class Researcher
     {
-        enum EmploymentLevel { A, B, C, D, E };
-        enum Campus { CradleCoast, Hobart, Launceston };
-        private string givenName, familyName, title, email, currentJobTitle;
+        public enum EmploymentLevel { Student, A, B, C, D, E };
+        public enum Campus { CradleCoast, Hobart, Launceston };
+        public enum Title { Dr, Mr, Mrs, Ms, Prof, Rev };
+        private string givenName, familyName, email, currentJobTitle;
         private DateTime commencedInstitution, commencedPosition;
         private double tenure, q1Percentage;
         private int publications;
+        private EmploymentLevel employmentLevel;
+        private Campus campus;
+        private Title title; 
 
+        public string ToBasicName()
+        { 
+            return familyName + ", " + givenName + "(" + title + ")";
+        }
+
+        public Researcher(Title title, string givenName, string familyName)
+        {
+            this.givenName = givenName;
+            this.familyName = familyName;
+            this.title = title;
+        } 
     }
 }
