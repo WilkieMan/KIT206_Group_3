0308f37f7ed85d336ca2f64517f4acf631327814
TishaSumi <132541836+TishaSumi@users.noreply.github.com>
2023-12-21 16:08:26 +1100

    Update Staff.cs

--- KIT206_RAP/KIT206_RAP/Staff.cs
+++ KIT206_RAP/KIT206_RAP/Staff.cs
@@ -7,6 +7,65 @@ using static KIT206_RAP.Researcher;
 
 namespace KIT206_RAP
 {
+      namespace Researcher
+    {
+        //Three year average of publications for reseacher
+
+        public double threeYearAverage
+        {
+            get
+            {
+                double threeYearPublicationCount = 0.0;
+
+                foreach (Publication t in Publications)
+                {
+                    if ((t.Year >= (DateTime.Today.Year - 3)) && (t.Year <= (DateTime.Today.Year - 1)))
+                    {
+                        threeYearPublicationCount++;
+                    }
+                }
+
+                return Math.Round(threeYearPublicationCount / 3, 1);
+            }
+        }
+
+        //Performance of researcher 
+
+        public double getPerformance
+        {
+            get
+            {
+                if (Type == "Staff")
+                {
+                    double realPublications = threeYearAverage;
+                    double expectedPublications;
+
+                    switch (Level)
+                    {
+                        case EmploymentLevel.A:
+                            expectedPublications = 0.5;
+                            break;
+                        case EmploymentLevel.B:
+                            expectedPublications = 1;
+                            break;
+                        case EmploymentLevel.C:
+                            expectedPublications = 2;
+                            break;
+                        case EmploymentLevel.D:
+                            expectedPublications = 3;
+                            break;
+                        default:
+                            expectedPublications = 4;
+                            break;
+                    }
+
+                    return (Math.Round(100 * (realPublications / expectedPublications), 1));
+                }
+                else
+                {
+                    return 0;
+                }
+            }
     internal class Staff : Researcher
     {
         public enum Performance {A, B, C, D, E }
