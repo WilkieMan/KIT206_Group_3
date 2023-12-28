e46a1774b00e8502b3a0651a6a0dcbdabe4102d7
TishaSumi <132541836+TishaSumi@users.noreply.github.com>
2023-12-28 16:46:18 +1100

    Update Position.cs

--- KIT206_RAP/KIT206_RAP/Position.cs
+++ KIT206_RAP/KIT206_RAP/Position.cs
@@ -6,18 +6,59 @@ using System.Threading.Tasks;
 
 namespace KIT206_RAP
 {
-    class Position
-    {
-       public EmploymentLevel Level { get; set; }
-       public DateTime Start { get; set; }
-       public DateTime End { get; set; }
-        
-       public enum EmploymentLevel { Student, A, B, C, D, E };
+    public class Position
+        {
+            public int ID { get; set; }                         //ID of researcher
+            public EmploymentLevel Level { get; set; }          //Employment level 
+            public DateTime Start { get; set; }                 //Start day 
+            public DateTime End { get; set; }                   //End day  
 
+            //To title of the position
+            public string ToTitle(EmploymentLevel el)
+            {
+                string title;                                   //The title of the researcher
+                switch (el)
+                {
+                    case EmploymentLevel.Student:
+                        title = "Student";
+                        break;
+                    case EmploymentLevel.A:
+                        title = "Research Associate";
+                        break;
+                    case EmploymentLevel.B:
+                        title = "Lecturer";
+                        break;
+                    case EmploymentLevel.C:
+                        title = "Assistant Professor";
+                        break;
+                    case EmploymentLevel.D:
+                        title = "Associate Professor";
+                        break;
+                    default:
+                        title = "Professor";
+                        break;
+                }
 
-       public string ToTitle()
-       {
-            return Level + " Start - " + Start + ", End - " + End;
-       }
-    }
+                return title;
+            }
+
+            //ToString method about the position
+            public override string ToString()
+            {
+                string start = Start.ToString("dd-MM-yyyy");  //The start date 
+                string end;                                   //The end date
+                string title = ToTitle(Level);                //Title of the posititon  
+
+                if (Date.Compare(End, Date.Today) == 0)
+                {
+                    end = "NULL";
+                }
+                else
+                {
+                    end = End.ToString("dd-MM-yyyy");
+                }
+                return String.Format("Start: {0}, End: {1},  {2}", start, end, title);
+            }
+
+        }
 }
